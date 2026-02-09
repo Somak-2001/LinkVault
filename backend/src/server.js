5d9cc2c6d19f67414ab209c670d90d5deea0bfcc
Somak Poddar <somakpoddar@gmail.com>
2026-02-10 03:56:56 +0530

    DB String Added and corresponding changes make

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,3 +1,6 @@
+import dotenv from "dotenv";
+dotenv.config();
+
 import app from "./app.js";
 import { connectDB } from "./config/db.js";
 
@@ -5,6 +8,6 @@ const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
   app.listen(PORT, () => {
-    console.log(`Backend running on http://localhost:${PORT}`);
+    console.log(`Server running on http://localhost:${PORT}`);
   });
 });
